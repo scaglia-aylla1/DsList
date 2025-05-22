d1f7626ec5c5167add4679230c8d6f9f7290579e
scaglia-aylla1 <aylla@scaglia.com.br>
2025-05-22 20:38:56 -0300

    :tada: Project Created

---
+++ src/test/java/com/scaglia/DsList/DsListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaglia.DsList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
